ec706093bbe90a19c89474738a5289635716a9ba
André F. Paiva <andrepaivadev@gmail.com>
2022-03-21 19:57:41 -0300

    atualização do curso

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -1,14 +1,14 @@
 class Tabelas {
     init(conexao) {
-        this.conexao = conexao;
-            console.log('tabelas foram chamadas')
+        this.conexao = conexao
+            
         this.criarAtendimentos();
     }
 
     criarAtendimentos() {
-        const sql = 'CREATE TABLE Atendimentos(id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, obsersacoes text, PRIMARY KEY(id))'
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos(id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, data datetime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, obsersacoes text, PRIMARY KEY(id))'
 
-        this.conexao.query(sql, (erro) => {
+        this.conexao.query(sql, erro => {
             if (erro) {
                 console.log(erro);
             } else {
